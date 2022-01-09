d7ea38f32c8a9f15567afd878dcaef5e9ec173e3
Tirta Styadi <tirta.setiady@yahoo.com>
2022-01-09 21:11:40 +1100

    Update the header

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,10 @@
-var timeDisplay = $('#time-display');
 var currentDay = $('#currentDay');
 
 
 //Display Real Time
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplay.text(rightNow);
+    currentDay.text(rightNow);
   }
 
 setInterval(displayTime, 1000);
